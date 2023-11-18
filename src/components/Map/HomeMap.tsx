39e9cea8de2700eb2ec9ced999aa61b28f593337
Nguyễn Văn Vương <93960884+NVanVuong@users.noreply.github.com>
2023-11-18 12:44:37 +0700

    fix Map

--- src/components/Map/HomeMap.tsx
+++ src/components/Map/HomeMap.tsx
@@ -13,8 +13,6 @@ interface MapProps {
 }
 
 const HomeMap: React.FC<MapProps> = ({ zoom = 20, dataRooms }) => {
-    console.log(dataRooms)
-
     const [center, setCenter] = useState([dataRooms[0].coordinate.latitude, dataRooms[0].coordinate.longitude])
     function MyMap() {
         const map = useMap()
--- src/container/Footer/index.tsx
+++ src/container/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     const currentYear = new Date().getFullYear()
 
     return (
-        <div className="mt-8 flex w-full items-center justify-start border-t border-gray-200 bg-gray-50 px-28 py-6 text-sm text-gray-700">
+        <div className="flex w-full items-center justify-start border-t border-gray-200 bg-gray-50 px-4 py-6 text-sm text-gray-700 sm:px-6 md:px-10 xl:px-28">
             © {currentYear} Rentally, Inc
         </div>
     )
