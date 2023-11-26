c92537b9b7cd4e8018a5f52de1fe606c19607993
Nguyễn Văn Vương <93960884+NVanVuong@users.noreply.github.com>
2023-11-26 20:56:56 +0700

    remove package-lock

--- src/container/Footer/index.tsx
+++ src/container/Footer/index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
 
     return (
         <div className="flex w-full items-center justify-start border-t border-gray-200 bg-gray-50 px-4 py-6 text-sm text-gray-700 sm:px-6 md:px-10 xl:px-28">
-            © {currentYear} Rentally, Inc
+            <span>© {currentYear} Rentally, Inc</span>
         </div>
     )
 }
