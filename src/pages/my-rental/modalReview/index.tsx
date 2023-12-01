f581160b77420d339723d8f0fa5815de1676193d
Nguyen Van Vuong <vuong.nguyen-van@internal.banvien.com>
2023-12-01 09:38:55 +0700

    UI review pending

--- src/pages/my-rental/modalReview/index.tsx
+++ src/pages/my-rental/modalReview/index.tsx
@@ -5,6 +5,8 @@ import { MODAL } from "@/utils/constants/GlobalConst"
 import ModalAntd from "@/components/Modal"
 import TextArea from "antd/es/input/TextArea"
 
+const desc = ["terrible", "bad", "normal", "good", "wonderful"]
+
 const ModalReview = () => {
     const onFinish = async (values: any) => {
         console.log(values)
@@ -18,24 +20,54 @@ const ModalReview = () => {
         <ModalAntd>
             <Spin spinning={false}>
                 <Title>Review room</Title>
-                <Form onFinish={onFinish} layout="vertical" className="flex w-full flex-col items-center gap-2">
-                    <div className="grid w-full grid-cols-1 gap-x-4 gap-y-2 md:grid-cols-2">
-                        <Form.Item label="Clean Rate" className="!w-full" name="cleanRate">
+                <Form onFinish={onFinish} layout="horizontal" className="flex w-full flex-col items-center gap-2">
+                    <div className="grid w-full grid-cols-1 gap-x-4 gap-y-2">
+                        <Form.Item
+                            label="Clean Rate"
+                            className="!w-full"
+                            name="cleanRate"
+                            labelCol={{ span: 8 }}
+                            labelAlign="left"
+                        >
                             <Rate defaultValue={5} />
                         </Form.Item>
-                        <Form.Item label="Support Rate" className="!w-full" name="supportRate">
+                        <Form.Item
+                            label="Support Rate"
+                            className="!w-full"
+                            name="supportRate"
+                            labelCol={{ span: 8 }}
+                            labelAlign="left"
+                        >
                             <Rate defaultValue={5} />
                         </Form.Item>
 
-                        <Form.Item label="Location Rate" className="!w-full" name="locationRate">
+                        <Form.Item
+                            label="Location Rate"
+                            className="!w-full"
+                            name="locationRate"
+                            labelCol={{ span: 8 }}
+                            labelAlign="left"
+                        >
                             <Rate defaultValue={5} />
                         </Form.Item>
-                        <Form.Item label="Security Rate" className="!w-full" name="securityRate">
+                        <Form.Item
+                            label="Security Rate"
+                            className="!w-full"
+                            name="securityRate"
+                            labelCol={{ span: 8 }}
+                            labelAlign="left"
+                        >
                             <Rate defaultValue={5} />
                         </Form.Item>
                     </div>
-                    <Form.Item label="Comment" className="!w-full" name="comment">
-                        <TextArea rows={4} />
+                    <Form.Item
+                        label="Comment"
+                        className="!w-full"
+                        name="comment"
+                        labelAlign="left"
+                        labelCol={{ span: 24 }}
+                    >
+                        <TextArea rows={4} style={{ display: "block", width: "100%" }} />
                     </Form.Item>
 
                     <Form.Item className="w-full">
